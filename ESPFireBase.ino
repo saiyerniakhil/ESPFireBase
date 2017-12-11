61d191a6737a28a1e681c7ade449731f31116bda
Sai Yerni Akhil <32711472+saiyerniakhil@users.noreply.github.com>
2017-12-11 20:43:33 +0530

    Initial commit

---
+++ ESPFireBase.ino
@@ -0,0 +1,49 @@
+#include <ESP8266WiFi.h>
+#include<FirebaseArduino.h>
+#define FIREBASE_HOST "xxxxxxxxxxxxxxxxxx.firebaseio.com" //Your Firebase Project URL goes here without "http:" , "\" and "/"
+#define FIREBASE_AUTH "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" //Your Firebase Database Secret goes here
+#define WIFI_SSID "xxxxxxxxxx" //your WiFi SSID for which yout NodeMCU connects
+#define WIFI_PASSWORD "12akhil!"//Password of your wifi network 
+int Ledpin=D4;//Inbuilt LED 
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200); //baud rate ,if you want to see the process in the serial monitor ,same baud rate should be set.
+  pinMode(Ledpin,OUTPUT);
+  WiFi.begin(WIFI_SSID,WIFI_PASSWORD);
+  Serial.print("connecting");
+  while (WiFi.status()!=WL_CONNECTED){
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+  Serial.print("connected:");
+  Serial.println(WiFi.localIP());
+
+  Firebase.begin(FIREBASE_HOST,FIREBASE_AUTH);
+  Firebase.set("S",0);//the variable in which is used in our Firebase and MIT App Inventor 
+}
+void firebasereconnect()
+{
+  Serial.println("Trying to reconnect");
+    Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  }
+int l;
+void loop() {
+  if (Firebase.failed()) {
+      Serial.print("setting number failed:");
+      Serial.println(Firebase.error());  
+      firebasereconnect();
+      return;
+  }
+  l=Firebase.getString("S").toInt();//The value read from the firebase is read in the form of String and is converted into Integer
+  if(l==1){
+      digitalWrite(Ledpin,HIGH);
+      Serial.println("lightON");
+    }
+    else if(l==0){
+      digitalWrite(Ledpin,LOW);
+      Serial.println("lightOFF");
+    }
+}
+
+
---
+++ Readme.txt
@@ -0,0 +1,15 @@
+ESP8266 1.0 12E Controlled With Firebase
+
+In this project , the Micro Controller used is NodeMCU ESP8266 1.0 12E Controlled Via An Android app made through MIT App Inventor Environment.
+
+This Project is developed on Windows 10 Professional 1709,Arduino IDE Version 1.8.4
+
+You can get Arduino from www.arduino.cc
+
+You can upload this peice of code into ESP8266 in arduino by clicking "Upload"
+
+Authors
+
+   Sai Yerni Akhil Madabattula - Initial work
+
+ This Code has been Inspired from Ashwin(Sir)
